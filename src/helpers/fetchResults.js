33617810af9a283b6b6b3f43f57a679596057563
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-25 06:59:56 +0100

    feat: create data fetching helper fn and hook

---
+++ src/helpers/fetchResults.js
@@ -0,0 +1,30 @@
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+function randomInteger(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+export default async function fetchResults() {
+  const LOADER_DURATION = randomInteger(400, 700);
+
+  try {
+    const response = await fetch("/data.json");
+    await sleep(LOADER_DURATION);
+
+    if (!response.ok) throw new Error("Network response was not ok.");
+
+    const data = await response.json();
+
+    if (!data || data.length === 0) throw new Error("Fetch failed");
+
+    return data;
+  } catch (error) {
+    console.error("Error occurred: ", error);
+    throw error;
+  }
+}
